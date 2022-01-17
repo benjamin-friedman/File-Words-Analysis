7445abfab0c6592db5ff808bb97d2b8f6dda0c44
benjamin-friedman <82235512+benjamin-friedman@users.noreply.github.com>
2022-01-16 22:59:15 -0500

    Update hash_table.c

--- Program Files/hash_table.c
+++ Program Files/hash_table.c
@@ -1,5 +1,5 @@
 /*
-   Copyright (C) 2020 Benjamin G. Friedman. Code may be used or redistributed freely with credit given to the author.
+   Copyright (C) 2020 Benjamin G. Friedman
    File Description:
 	- name: hash_table.c
 	- description: implementation file for the hash table interface.
