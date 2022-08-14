25651e1dabc3a917db6ceb0e5734082955211f08
benjamin-friedman <82235512+benjamin-friedman@users.noreply.github.com>
2022-08-14 16:52:16 -0400

    Create Status.h

---
+++ ProgramFiles/Status.h
@@ -0,0 +1,18 @@
+/*
+  Author: Benjamin G. Friedman
+  Date: 12/25/20
+  File: Status.h
+  Description:
+      - Header file for the Status and Boolean enumerated types.
+*/
+
+
+#ifndef STATUS_H
+#define STATUS_H
+
+
+typedef enum status { FAILURE, SUCCESS } Status;
+typedef enum boolean { FALSE, TRUE } Boolean;
+
+
+#endif
